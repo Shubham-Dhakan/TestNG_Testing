ade8233ea0149453d1302f3f332dfd3b918a28a1
shubham <shubham.dhakan@gmail.com>
2022-10-19 16:13:44 -0400

    adding updated files to local repo

--- Testing_Day1.java
+++ Testing_Day1.java
@@ -1,5 +1,8 @@
-package qaTesting;
+package testPackage2;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Testing_Day1 {
@@ -9,19 +12,19 @@ public void testing_seleniumJava() {
 	System.out.println("Automated message from TestNG, performing selenium testing"); 
 }
 	
-@Test
+@BeforeTest
 public void testing_appium() {
-	System.out.println("Automated message from TestNG, performing appium mobile testing");
+	System.out.println("Automated message from TestNG, performing appium mobile testing: I will get executed first of all tests.");
 }
 
-@Test
+@AfterTest
 public void testing_restapi() {
-	System.out.println("Automated message from TestNG, performing REST api testing");
+	System.out.println("Automated message from TestNG, performing REST api testing: I will get executed after all tests.");
 }
 
-@Test
+@BeforeMethod
 public void testing_soapui() {
-	System.out.println("Automated message from TestNG, performing SOAP UI testing");
+	System.out.println("Automated message from TestNG, performing SOAP UI testing and i will get printed before once before all the testcases.");
 }
 
 @Test 
